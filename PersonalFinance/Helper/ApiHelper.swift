24753277024d1d0a3dd79ad5942d7a979424ec66
dgwu <danielgunawanwu@gmail.com>
2018-12-11 15:41:35 +0700

    add income expense page as project placeholder

---
+++ PersonalFinance/Helper/ApiHelper.swift
@@ -0,0 +1,12 @@
+//
+//  ApiHelper.swift
+//  PersonalFinance
+//
+//  Created by Daniel Gunawan on 11/12/18.
+//  Copyright © 2018 Daniel Gunawan. All rights reserved.
+//
+
+import Foundation
+class ApiHelper {
+    // implement later
+}
