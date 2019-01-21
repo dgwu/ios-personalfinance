3416d8af52f7ae75fb81f209978be5de1091407a
okky pribadi <donbosco_okky@yahoo.com>
2019-01-21 12:02:01 +0700

    chek 1

--- PersonalFinance/Setting/Currency/CurrencyViewController.swift
+++ PersonalFinance/Setting/Currency/CurrencyViewController.swift
@@ -8,14 +8,30 @@
 
 import UIKit
 
-class CurrencyViewController: UIViewController {
+class CurrencyViewController: TableViewController
+{
 
+    let currency = ["IDR","ENG"]
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
-
-       
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return currency.count
+    }
+    
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+//    {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+//        cell.textLabel?.text = currency[indexPath.row]
+//        return cell
+//    }
+    
+    
+    
     
     
     
